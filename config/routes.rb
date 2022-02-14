2d0ae98502c47a0ffb569cc094e8a017bdb739d3
Marcus Baw <marcusbaw@gmail.com>
2022-02-14 16:30:22 +0000

    bootstrap and simple nav

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
+  get 'pages/about'
+  get 'pages/home'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  root "pages#home"
 end
